25458ba4a86622a857dbcc0bb81f995a917f63d4
Shusanto Kumar Modak <shusanto294@gmail.com>
2025-07-22 00:50:24 +0600

    Production login issue fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production' && process.env.HTTPS === 'true',
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 }));
